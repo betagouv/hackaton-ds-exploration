964f47f3bd8783a9de1d0ac457c1e32580cda4c6
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-09-09 14:41:29 +0200

    count max dossiers

--- main.rb
+++ main.rb
@@ -20,11 +20,18 @@ data_with_dossiercount = data.select{|d| d['dossiersCount'] > 0}
 puts "#{p_number data_with_dossiercount.count} Démarches avec au moins un dossier"
 # json_pp data_with_dossiercount.first
 
+# EXPLORE REVISIONS KEYS ""
 # data_grouped_by_revisions_keys_count = data_with_dossiercount.inject(Hash.new(0)) { |h, e| h[e['revision'].keys.first] += 1 ; h }
 # pp data_grouped_by_revisions_keys_count
 # {"champDescriptors"=>19138} (OK there is always one key)
 
-data_grouped_by_champDescriptors_count = data_with_dossiercount.inject(Hash.new(0)) { |h, e| h[e['revision']['champDescriptors'].count] += 1 ; h }.sort.to_h
-puts "Nombre de champs => nombre de dossiers"
-pp data_grouped_by_champDescriptors_count
+# COUNT PER AMOUNT OF FIELDS #
+# data_grouped_by_champDescriptors_count = data_with_dossiercount.inject(Hash.new(0)) { |h, e| h[e['revision']['champDescriptors'].count] += 1 ; h }.sort.to_h
+# puts "Nombre de champs => nombre de dossiers"
+# pp data_grouped_by_champDescriptors_count
 # There is between 1 and ~120 fields
+
+# COUNT PER DOSSIER #
+data_per_dossiercount = data.inject(Hash.new(0)) { |h, e| h[e['dossiersCount']] += 1 ; h }.sort.to_h
+# pp data_per_dossiercount
+puts "démarche avec le plus de dossiers : #{p_number data_per_dossiercount.keys.max}"
