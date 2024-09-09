7ef425527b2d07d478d20338acb37e491fd9c05a
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-09-09 14:24:12 +0200

    extract first record with dossiers

--- main.rb
+++ main.rb
@@ -1,12 +1,21 @@
 require 'json'
 
+def json_pp(h)
+  puts JSON.pretty_generate(h)
+end
+
+def p_number(n)
+  n.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+end
+
 DATA_PATH = '../20240901041013-demarches.json'
 
 file = File.read(DATA_PATH)
 data = JSON.parse(file)
 
-def json_pp(h)
-  puts JSON.pretty_generate(h)
-end
+p "#{p_number data.count} Démarches au total"
+# json_pp data.first
 
-json_pp data.first
+data_with_dossiercount = data.select{|d| d['dossiersCount'] > 0}
+p "#{p_number data_with_dossiercount.count} Démarches avec au moins un dossier"
+# json_pp data_with_dossiercount.first
