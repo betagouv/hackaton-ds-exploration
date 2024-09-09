e30b71b74fe71aaba767708354686bf0849f7d4a
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-09-09 14:35:32 +0200

    explore the number of fields in champDescriptors

--- main.rb
+++ main.rb
@@ -20,3 +20,11 @@ data_with_dossiercount = data.select{|d| d['dossiersCount'] > 0}
 puts "#{p_number data_with_dossiercount.count} Démarches avec au moins un dossier"
 # json_pp data_with_dossiercount.first
 
+# data_grouped_by_revisions_keys_count = data_with_dossiercount.inject(Hash.new(0)) { |h, e| h[e['revision'].keys.first] += 1 ; h }
+# pp data_grouped_by_revisions_keys_count
+# {"champDescriptors"=>19138} (OK there is always one key)
+
+data_grouped_by_champDescriptors_count = data_with_dossiercount.inject(Hash.new(0)) { |h, e| h[e['revision']['champDescriptors'].count] += 1 ; h }.sort.to_h
+puts "Nombre de champs => nombre de dossiers"
+pp data_grouped_by_champDescriptors_count
+# There is between 1 and ~120 fields
