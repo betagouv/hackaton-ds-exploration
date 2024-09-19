83045b01b69c4802988fa53cb04291ca9146d958
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-09-19 14:19:12 +0200

    generate csv summary for better lisibility

--- app/search_summary.rb
+++ app/search_summary.rb
@@ -1,8 +1,10 @@
 require 'json'
+require 'csv'
 
 class SearchSummary
   SEARCH_FOLDER = 'search_attachments'
-  RESULT_FILE = 'search_summary.json'
+  RESULT_JSON_FILE = 'search_summary.json'
+  RESULT_CSV_FILE = 'search_summary.csv'
 
   def summarize
     all_searches = []
@@ -19,9 +21,22 @@ class SearchSummary
       .sort_by{ |d| -d['total_dossiers_count'] }
       .map{ |d| d.except 'results' }
       
-    File.open(RESULT_FILE, 'w') do |f|
+    File.open(RESULT_JSON_FILE, 'w') do |f|
       f.write(JSON.pretty_generate(summary))
     end
+
+    File.write(RESULT_CSV_FILE, csv_summary(summary))
+
     puts "Summary updated."
+  end
+
+  def csv_summary(summary)
+    column_names = summary.first.keys
+    csv_string = CSV.generate do |csv|
+      csv << column_names
+      summary.each do |search_summary|
+        csv << search_summary.values
+      end
+    end
   end
 end
