c4ce3e609867d093af4ee3dcbf7d8050652e6e50
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-10-01 11:03:14 +0200

    more searches and grouped results

---
+++ group_results.rb
@@ -0,0 +1,27 @@
+require 'json'
+require 'csv'
+
+filename = ARGV[0]
+file = File.read(filename)
+data = JSON.parse(file)
+
+grouped_results = data["results"].reduce({}) do |h, result|
+  orga = result["organisme"]
+  h[orga] = {demarches: 0, dossiers: 0} unless h[orga]
+  
+  h[orga][:demarches] += 1
+  h[orga][:dossiers] += result["dossiersCount"]
+  h
+end
+
+# puts grouped_results
+
+csv_string = CSV.generate do |csv|
+  csv << ["orga", *grouped_results.values.first.keys]
+
+  grouped_results.each do |orga, result|
+    csv << [orga, *result.values]
+  end
+end
+
+puts csv_string
